8b373e54068d223abfaf5f46e9a1afe52776bd64
Luca Park <qlife1146@gmail.com>
2023-01-10 15:49:26 +0900

    first init

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, SafeAreaView, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+//왜 그냥 SCREEN_WIDTH = Dimension ...은 안되지??
+//아 뒤에 width 추가해야되네;;;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.city}>
+                <Text style={styles.cityName}>Gimpo</Text>
+            </View>
+            <ScrollView
+                pagingEnabled
+                horizontal
+                contentContainerStyle={styles.weather}
+            >
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27℃</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27℃</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27℃</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27℃</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27℃</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27℃</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "tomato",
+    },
+    city: {
+        flex: 1,
+        // backgroundColor: "blue",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    cityName: {
+        fontSize: 100,
+        fontWeight: "600",
+    },
+    weather: {
+        backgroundColor: "blue",
+    },
+    day: {
+        width: SCREEN_WIDTH,
+        alignItems: "center",
+    },
+    temp: {
+        fontSize: 200,
+        fontWeight: "600",
+    },
+    description: {
+        fontSize: 100,
+        fontWeight: "400",
+    },
 });
---
+++ readme.md
@@ -0,0 +1,7 @@
+## 개요
+
+React Native 공부용.
+
+## 새로 알게된 점
+
+-   `<div>`가 아닌 `<View>`, `<p>`가 아닌 `<Text>` ~~flutter랑 비슷하네~~
